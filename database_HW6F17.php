4d4b4544bb61f934f2fc5d50c68b9921c2754425
Jinhao Chen <jinhaochen@x-134-84-1-72.vpn.umn.edu>
2017-12-30 19:32:12 +0800

    synfromJinhao@MBP

--- database_HW6F17.php
+++
@@ -1,8 +0,0 @@
-<?php
-      $db_servername = 'cse-curly.cse.umn.edu';
-      $db_port = 3306;
-      $db_name = 'F17CS4131U15';  //Note is a  number, you can find it on moodle
-      $db_username = 'F17CS4131U15';    //xxx is the number assigned on the moodle
-      $db_password = 'mypasswordis8507';  // provided in moodle.
-      $db = mysqli_connect($db_servername,$db_username,$db_password,$db_name,$db_port);
-?>
--- delete_ca.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $filename = "calendar.txt";
-    unlink($filename);
-    // //Redirect to calendar.php
-    echo '<script type="text/javascript"> window.location.href = "calendar.php" </script>'
-?>
